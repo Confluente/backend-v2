b40631fa0e39680bdc0e602c692ce1a6fa94e222
HiddeKoerts <hiddekoerts@live.nl>
2020-08-14 22:06:21 +0200

    TypeScript support and hello world server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const express = require( "express" );
+const app = express();
+const port = 3000; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req: any, res: { send: (arg0: string) => void; } ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
