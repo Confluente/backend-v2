21ffc9339a4fcd73b785559dd7b580fc209f974e
H.O. Koerts <hiddekoerts@live.nl>
2020-12-04 23:00:35 +0100

    Added notifications (first route)

---
+++ src/routes/notifications.ts
@@ -0,0 +1,26 @@
+import express = require("express");
+import User = require("../models/user");
+
+const router: any = express.Router();
+
+router.route("/portraitRight/:id")
+    /**
+     * Function for changing the consentWithPortraitRight attribute of a user
+     */
+    .put(function(req: any, res: any): any {
+        // Check if user is logged in
+        const userId: number = res.locals.session ? res.locals.session.user : null;
+
+        // Check if user id of logged in user is the same as user id for which request was send
+        if (parseInt(req.params.id, undefined) !== userId) { return res.sendStatus(403); }
+
+        // Retrieve user from database
+        User.findByPk(userId).then(function(user: User): any {
+            // Update user object in database
+            user.update({consentWithPortraitRight: req.body.answer}).then(function(result: User): any {
+                res.send(user);
+            });
+        });
+    });
+
+module.exports = router;
