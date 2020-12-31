82d89f538bbbece78fc6eafa28da52c27ea0dce9
H.O. Koerts <hiddekoerts@live.nl>
2020-12-31 18:12:40 +0100

    Changed permissions to individual attributes of model to force declaration for all roles

--- src/models/role.ts
+++ src/models/role.ts
@@ -7,32 +7,60 @@ export class Role extends sequelize.Model {
     public name!: string;
 
     /**
-     * Stringified dictionary of permissions associated to the role.
+     * Permission to view pages.
      */
-    public permissions!: string;
-}
+    public PAGE_VIEW!: boolean;
 
-const defaultPermissions = {
-    // Pages
-    PAGE_VIEW: true,
-    PAGE_MANAGE: false,
-    // Users
-    USER_CREATE: true,
-    USER_VIEW_ALL: false,
-    USER_MANAGE: false,
-    CHANGE_ALL_PASSWORDS: false,
-    // Roles
-    ROLE_VIEW: false,
-    ROLE_MANAGE: false,
-    // Groups
-    GROUP_VIEW: true,
-    GROUP_MANAGE: false,
-    GROUP_ORGANIZE_WITH_ALL: false,
-    // Activities
-    ACTIVITY_VIEW_PUBLISHED: true,
-    ACTIVITY_VIEW_ALL_UNPUBLISHED: false,
-    ACTIVITY_MANAGE: false
-};
+    /**
+     * Permission to manage pages.
+     */
+    public PAGE_MANAGE!: boolean;
+
+    /**
+     * Permission to create users.
+     */
+    public USER_CREATE!: boolean;
+
+    /**
+     * Permission to view all users.
+     */
+    public USER_VIEW_ALL!: boolean;
+
+    /**
+     * Permission to manage users.
+     */
+    public USER_MANAGE!: boolean;
+
+    /**
+     * Permission to change the passwords of all accounts.
+     */
+    public CHANGE_ALL_PASSWORDS!: boolean;
+
+    /**
+     * Permission to view roles.
+     */
+    public ROLE_VIEW!: boolean;
+
+    /**
+     * Permission to manage roles.
+     */
+    public ROLE_MANAGE!: boolean;
+
+    /**
+     * Permission to view published activities.
+     */
+    public ACTIVITY_VIEW_PUBLISHED!: boolean;
+
+    /**
+     * Permission to view all unpublished activities.
+     */
+    public ACTIVITY_VIEW_ALL_UNPUBLISHED!: boolean;
+
+    /**
+     * Permission to manage activities.
+     */
+    public ACTIVITY_MANAGE!: boolean;
+}
 
 Role.init(
     {
@@ -42,13 +70,104 @@ Role.init(
             primaryKey: true,
             allowNull: false,
         },
-        permissions: {
-            type: new sequelize.DataTypes.STRING(1024),
+        PAGE_VIEW: {
+            type: new sequelize.DataTypes.BOOLEAN(),
             unique: false,
             primaryKey: false,
             allowNull: false,
-            defaultValue: stringifyDictionaryOfBooleans(defaultPermissions),
-        }
+            defaultValue: false,
+        },
+        PAGE_MANAGE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        USER_CREATE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        USER_VIEW_ALL: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        USER_MANAGE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        CHANGE_ALL_PASSWORDS: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        ROLE_VIEW: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        ROLE_MANAGE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        GROUP_VIEW: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        GROUP_MANAGE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        GROUP_ORGANIZE_WITH_ALL: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        ACTIVITY_VIEW_PUBLISHED: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        ACTIVITY_VIEW_ALL_UNPUBLISHED: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
+        ACTIVITY_MANAGE: {
+            type: new sequelize.DataTypes.BOOLEAN(),
+            unique: false,
+            primaryKey: false,
+            allowNull: false,
+            defaultValue: false,
+        },
     }
 );
 
