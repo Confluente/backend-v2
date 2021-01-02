7e9ca3f95bd801d62b18331b596b173f95e67472
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-01-02 15:52:06 +0100

    redefine database with seq-ts

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,6 +1,13 @@
-import {Sequelize} from 'sequelize';
+import {Sequelize} from 'sequelize-typescript';
 
 const storage = (process.env.NODE_ENV === "test") ? ":memory:" : "data.sqlite";
 
-export let db: Sequelize = new Sequelize("sequel", null, null, {dialect: "sqlite", logging: null, storage});
+export const db: Sequelize = new Sequelize({
+    database: "db",
+    dialect: "sqlite",
+    username: null,
+    password: null,
+    storage,
+    models: [__dirname + '/models']
+});
 
