93272e835b6eb394fce3876fdba2e481ae15d63a
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-01-30 12:21:48 +0100

    more tests

--- test/helpers/auth.helper.test.ts
+++ test/helpers/auth.helper.test.ts
@@ -1,9 +1,22 @@
 import {getPasswordHash} from "../../src/helpers/auth.helper";
-import { expect } from "chai";
+import { expect, assert } from "chai";
 
 describe("auth.helper.ts", () => {
 
     describe("getPasswordHash", () => {
+
+        it("basic case", (done) => {
+            const password = "s;lksdjf;lskdjf";
+            const salt = "hahahaha";
+
+            getPasswordHash(password, salt).then(function(buf: Buffer): void {
+                if (JSON.stringify(buf) === "{\"type\":\"Buffer\",\"data\":[139,64,166,79,252,104,200,22,235,33,149,53,1,154,118,206,253,158,195,173,218,201,103,53,95,84,121,45,133,20,161,173]}") {
+                    done();
+                } else {
+                    done(new Error("Did not generate correct password hash!"));
+                }
+            });
+        });
 
         it("salt === null", () => {
             const password = "superduperpass";
@@ -20,7 +33,7 @@ describe("auth.helper.ts", () => {
 
             expect(() => {
                 getPasswordHash(password, salt);
-            }).to.throw("auth.helper.getPasswordHash: password was null.")
+            }).to.throw("auth.helper.getPasswordHash: password was null.");
         });
     });
 
