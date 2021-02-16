79837d924553bc5697ec96abf292ec07f27125bb
H.O. Koerts <hiddekoerts@live.nl>
2021-02-16 17:00:27 +0100

    Broken test

--- test/routes/page.route.test.ts
+++ test/routes/page.route.test.ts
@@ -1,6 +1,6 @@
 import {TestFactory} from "../testFactory";
 import {page} from "../test.data";
-import {cleanPages} from "../test.helper";
+import {cleanDb, cleanPages} from "../test.helper";
 import {Page} from "../../src/models/database/page.model";
 import {roleNotLoggedIn} from "../test.data";
 import {cleanRoles} from "../test.helper";
@@ -17,13 +17,14 @@ describe("page.route.ts", () => {
      * Syncs the database and server before all tests.
      */
     before(async () => {
-        await factory.init();
+        await factory.init(true);
     });
 
     /**
      * Closes database and server after all tests.
      */
     after(async () => {
+        await cleanDb();
         await factory.close();
     });
 
@@ -31,11 +32,13 @@ describe("page.route.ts", () => {
      * Checks if retrieving all pages works.
      */
     describe("Retrieving all pages", () => {
-        it("Returns error without proper permissions", async (done) => {
-            Role.create(roleNotLoggedIn);
-
-            const res = await factory.app.get("/page/");
-            done();
+        it("Returns error without proper permissions", (done) => {
+            factory.agents.nobodyUserAgent.get("/page/")
+                .expect(403).then(function(_: any): any {
+                done();
+            }).catch(function(_: any): any {
+                done(new Error());
+            });
         });
     });
 });
