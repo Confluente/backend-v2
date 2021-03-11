42feb6eae5114121258f5d385e2d5f8dffe11541
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-03-11 16:54:24 +0100

    more validation functions

--- src/helpers/type.validation.helper.ts
+++ src/helpers/type.validation.helper.ts
@@ -6,14 +6,32 @@ export const stringValidation = (data: any) => {
     }
 };
 
+export const stringValidationOrNull = (data: any) => {
+    if (typeof data !== "string" && data !== null) {
+        throw new ValidationError("Table column expected string (or null) but received " + (typeof data).toString());
+    }
+};
+
 export const booleanValidation = (data: any) => {
     if (typeof data !== "boolean") {
         throw new ValidationError("Table column expected boolean but received " + (typeof data).toString());
     }
 };
 
-export const stringValidationOrNull = (data: any) => {
-    if (typeof data !== "string" && data !== null) {
-        throw new ValidationError("Table column expected string (or null) but received " + (typeof data).toString());
+export const booleanValidationOrNull = (data: any) => {
+    if (typeof data !== "boolean" && data !== null) {
+        throw new ValidationError("Table column expected boolean(or null) but received " + (typeof data).toString());
+    }
+};
+
+export const numberValidation = (data: any) => {
+    if (typeof data !== "number") {
+        throw new ValidationError("Table column expected number but received " + (typeof data).toString());
+    }
+};
+
+export const numberValidationOrNull = (data: any) => {
+    if (typeof data !== "number" && data !== null) {
+        throw new ValidationError("Table column expected number (or null) but received " + (typeof data).toString());
     }
 };
