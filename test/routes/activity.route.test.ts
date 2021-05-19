06a3007ac45fa6459738ce1a647ccee1fd3e1f15
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-05-19 21:12:06 +0200

    layout of tests

--- test/routes/activity.route.test.ts
+++ test/routes/activity.route.test.ts
@@ -17,4 +17,64 @@ describe("activity.route.ts '/api/activities'", () => {
     after(async () => {
         await factory.close();
     });
+
+    describe("/", () => {
+
+        describe("get", () => {
+
+        });
+
+        describe("post", () => {
+
+        });
+
+    });
+
+    describe("/pictures/:id", () => {
+
+        describe("post", () => {
+
+        });
+
+        describe("put", () => {
+
+        });
+
+    });
+
+    describe("/manage", () => {
+
+        describe("get", () => {
+
+        });
+
+    });
+
+    describe("/subscriptions/:id", () => {
+
+        describe("post", () => {
+
+        });
+
+        describe("delete", () => {
+
+        });
+
+    });
+
+    describe("/:id", () => {
+
+        describe("get", () => {
+
+        });
+
+        describe("put", () => {
+
+        });
+
+        describe("delete", () => {
+
+        });
+
+    });
 });
